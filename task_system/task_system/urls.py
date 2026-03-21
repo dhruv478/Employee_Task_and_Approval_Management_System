77180b2fe5aad55c27046c6673ba8e068affe249
Dhruv Prajapati <dhruvprajapati10076@gmail.com>
2026-03-21 14:47:37 +0530

    Add gome API endpoinr

--- task_system/task_system/urls.py
+++ task_system/task_system/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
-
+from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/login/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/auth/login/',TokenRefreshView.as_view(),name='token_refresh'),
     path('api/auth/',include('accounts.urls')),
     path('api/',include('tasks.urls')),
+    path('',home),
 ]
